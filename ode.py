bbc6daf9a46c183c0369e69017274a11f37561ad
Mike Blanchard <mblancha@highpoint.edu>
2022-02-17 09:04:01 -0500

    code

--- ode.py
+++ ode.py
@@ -25,4 +25,3 @@ def RK4(diffeq, y0, t, h):
     k3 = h*diffeq(y0+0.5*k2, t + h/2.)      # dy/dt at t+h/2
     k4 = h*diffeq(y0+k3, t + h)             # dy/dt at t+h
     return y0 + (k1+k4)/6.0 + (k2+k3)/3.0
-    
